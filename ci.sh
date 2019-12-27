ba7936cc94301ed63ef5e78ce93555141ad1702c
Mark Rousskov <mark.simulacrum@gmail.com>
2019-12-27 16:43:36 -0500

    Apply rust-lang migration

--- ci.sh
+++ ci.sh
@@ -26,7 +26,7 @@ IFS=$'\n\t'
 GIT_COMMIT_MESSAGE="Automatic stats update"
 GIT_EMAIL="7378925+stats-updater@users.noreply.github.com"
 GIT_NAME="stats updater"
-GIT_REPO="rust-lang-nursery/rustc-pr-tracking"
+GIT_REPO="rust-lang/rustc-pr-tracking"
 GIT_BRANCH="master"
 
 
